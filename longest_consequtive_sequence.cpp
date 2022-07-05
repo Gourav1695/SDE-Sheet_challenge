18a6071f4307c32293d822de471f23f65e1ded3a
Gourav Kumar Shaw <97777989+Gourav1695@users.noreply.github.com>
2022-07-05 10:05:48 +0530

    Create longest_consequtive_sequence.cpp

---
+++ longest_consequtive_sequence.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+   
+  int longestConsecutive(vector<int>& nums) {
+	if(!size(nums))
+    return 0;
+	sort(begin(nums), end(nums));
+	int longest = 0, cur_longest = 1;
+	for(int i = 1; i < size(nums); i++) 
+		if(nums[i] == nums[i - 1]) continue;
+		else if(nums[i] == nums[i - 1] + 1) cur_longest++; // consecutive element - update current streak length
+		else longest = max(longest, cur_longest), cur_longest = 1;  // reset current streak length
+	return max(longest, cur_longest);
+  }  
+    
+};
