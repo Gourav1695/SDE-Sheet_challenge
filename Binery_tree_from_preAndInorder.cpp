ef3b964a9f5bf9a780b242e5ae4568cc9fd07c6e
Gourav Kumar Shaw <97777989+Gourav1695@users.noreply.github.com>
2022-07-14 17:24:55 +0530

    Create Binery_tree_from_preAndInorder.cpp

---
+++ Binery_tree_from_preAndInorder.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    TreeNode* buildTree(vector<int>& P, vector<int>& I) {
+        unordered_map<int, int> M;
+        for (int i = 0; i < I.size(); i++)
+            M[I[i]] = i;
+        return splitTree(P, M, 0, 0, I.size()-1);
+    }
+    
+private:
+    TreeNode* splitTree(vector<int>& P, unordered_map<int, int>& M, int pix, int ileft, int iright) {
+        int rval = P[pix], imid = M[rval];
+        TreeNode* root = new TreeNode(rval);            
+        if (imid > ileft)
+            root->left = splitTree(P, M, pix+1, ileft, imid-1);
+        if (imid < iright)
+            root->right = splitTree(P, M, pix+imid-ileft+1, imid+1, iright);
+        return root;
+    }
+};
