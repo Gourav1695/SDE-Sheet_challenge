99705f792037d55d251c71ff91be258776c3e473
Gourav Kumar Shaw <97777989+Gourav1695@users.noreply.github.com>
2022-08-04 20:41:41 +0530

    Create Mirror_Reflection.cpp

---
+++ Mirror_Reflection.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int mirrorReflection(int p, int q) {
+       while (p%2==0 && q%2==0)
+       {
+           p=p/2;
+           q=q/2;
+       }
+        if(p%2!=0&& q%2==0)
+        {
+            return 0;
+            
+        }
+        else if( p%2==0 && q%2!=0)
+        {
+            return 2;
+        }
+        else if(p%2!=0 && q%2!=0)
+        {
+            return 1;
+        }
+        return -1;
+        
+    }
+};
