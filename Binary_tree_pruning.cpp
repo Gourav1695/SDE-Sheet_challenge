eeea2cb89cce3778f231e366455923769ba5a548
Gourav Kumar Shaw <97777989+Gourav1695@users.noreply.github.com>
2022-09-06 18:24:20 +0530

    Create Binary_tree_pruning.cpp

---
+++ Binary_tree_pruning.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    TreeNode* pruneTree(TreeNode* root) {
+        if ( root!=NULL)
+        {
+            root->left= pruneTree(root->left);
+            root->right= pruneTree(root->right);
+            if(root->val== 0 && root->left==NULL && root->right == NULL){
+                return NULL;
+            }
+        }
+        return root;
+        
+    }
+};
