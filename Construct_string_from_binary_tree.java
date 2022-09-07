e1fa0aa8d6207e01d602e2118e74f534bfceb31f
Gourav Kumar Shaw <97777989+Gourav1695@users.noreply.github.com>
2022-09-07 17:18:07 +0530

    99 day consistency : ) on 07 sept 2022

---
+++ Construct_string_from_binary_tree.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String tree2str(TreeNode t) {
+        if(t==null)
+            return "";
+        if(t.left==null && t.right == null)
+            return t.val+"";
+        if(t.right == null)
+            return t.val+"("+tree2str(t.left)+")";
+        return t.val+"("+tree2str(t.left)+")("+tree2str(t.right)+")";
+    }
+}
