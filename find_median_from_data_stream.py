5ed93d5b533c33db5d8943a86f5fd7b2a49e287d
Gourav Kumar Shaw <97777989+Gourav1695@users.noreply.github.com>
2022-11-12 17:21:26 +0530

    done this qn on 12 nov 2022 at 5:25pm

---
+++ find_median_from_data_stream.py
@@ -0,0 +1,14 @@
+from sortedcontainers import SortedList
+class MedianFinder:
+
+    def __init__(self):
+        self.s = SortedList()
+
+    def addNum(self, num: int) -> None:
+        self.s.add(num)
+
+    def findMedian(self) -> float:
+        l = len(self.s)
+        if l%2==0:
+            return (self.s[l//2-1]+self.s[l//2])/2
+        return self.s[l//2]
