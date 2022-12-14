86e68755b9109bf5f28420ae88a171969a5a83d7
Gourav Kumar Shaw <97777989+Gourav1695@users.noreply.github.com>
2022-12-14 17:54:52 +0530

    Create House_robber.cpp

---
+++ House_robber.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        if ( nums.size()<1) return 0;//base case
+        if(nums.size()==1)  return nums[0];//base case
+
+        vector <int>dp(nums.size(),0);
+        dp[0]=nums[0];
+        dp[1]= max(nums[0],nums[1]);
+        for (int i=2; i<nums.size();i++)
+        {
+            dp[i]= max(dp[i-2]+nums[i],dp[i-1]);
+        }
+        return dp[nums.size()-1];
+    }
+};
