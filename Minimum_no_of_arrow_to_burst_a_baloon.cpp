32e055676bb73117eb01a3525b51da239cc5c010
Gourav Kumar Shaw <97777989+Gourav1695@users.noreply.github.com>
2023-01-05 20:52:19 +0530

    Create Minimum_no_of_arrow_to_burst_a_baloon.cpp

---
+++ Minimum_no_of_arrow_to_burst_a_baloon.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    static bool comp(vector<int> &a, vector<int> &b)
+    {
+        return a[1]<b[1];
+    }
+    int findMinArrowShots(vector<vector<int>>& points) {
+        if(points.size()==0)
+            return 0;
+        // we are sorting  with the end value of the vector of vector
+        sort(points.begin(), points.end(), comp);
+        
+        int arrows = 1;
+        int last = points[0][1];
+        
+        for(int i=1; i<points.size(); i++)
+        {
+            if(points[i][0]>last)
+            {
+                arrows++;
+                last = points[i][1];
+            }
+            else{
+                // do nothing
+            }
+        }
+        // cout<<arrows<<endl;
+        return arrows;
+    }
+};
