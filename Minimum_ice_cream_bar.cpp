00ad2214797b6890e8ce6deba03bc946ba98ff17
Gourav Kumar Shaw <97777989+Gourav1695@users.noreply.github.com>
2023-01-06 20:57:57 +0530

    Create Minimum_ice_cream_bar.cpp

---
+++ Minimum_ice_cream_bar.cpp
@@ -0,0 +1,9 @@
+int maxIceCream(vector<int>& costs, int coins) {
+    sort(begin(costs), end(costs));
+    for (int i = 0; i < costs.size(); ++i)
+        if (coins >= costs[i])
+            coins -= costs[i];
+        else
+            return i;
+    return costs.size();
+}
