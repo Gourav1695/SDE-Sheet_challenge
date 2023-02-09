405b4a5f8bf384f17815996e5836ebf58f0fc089
Gourav Kumar Shaw <97777989+Gourav1695@users.noreply.github.com>
2023-02-09 20:14:07 +0530

    Create Reverse_words_in_given_string.cpp

---
+++ Reverse_words_in_given_string.cpp
@@ -0,0 +1,27 @@
+
+class Solution
+{
+    public:
+    //Function to reverse words in a given string.
+    string reverseWords(string S) 
+    { 
+        // code here
+        string temp="";
+        string result="";
+        for(int i=S.length()-1; i>=0;i--){
+            if(S[i]=='.'){
+                temp=temp+S[i];
+                result=result+temp;
+                temp="";
+            }
+            else{
+                temp=S[i]+temp;
+                
+            }
+            
+        }
+        result=result + temp;
+        return result;
+    } 
+    
+};
