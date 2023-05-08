56c2ff4538c6a87360d9c000bc3d99e9f6640da8
Gourav Kumar Shaw <97777989+Gourav1695@users.noreply.github.com>
2023-05-08 16:34:32 +0530

    Create Matrix_diagonal_sum.cpp

---
+++ Matrix_diagonal_sum.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int diagonalSum(vector<vector<int>>& mat) {
+        int n= mat.size();
+        int ans=0;
+        for(int i=0; i<n;i++){
+            ans+=mat[i][i];
+            ans+=mat[n-1-i][i];
+
+        }
+        if(n%2!=0){
+            ans-=mat[n/2][n/2];
+        }
+        return ans;
+    }
+};
+
+// time complexicity -> O(N), N is no. of element in the matrix
+// space complexicity -> O(1), as we are using constant space;
