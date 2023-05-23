f9cd2329826148e3568efd2f47c17f4ab5fa9368
Gourav Kumar Shaw <97777989+Gourav1695@users.noreply.github.com>
2023-05-23 20:56:14 +0530

    done time and space complexicity

--- Kth_largest_element_in_stream.cpp
+++ Kth_largest_element_in_stream.cpp
@@ -1,27 +1,29 @@
 class KthLargest {
 public:
 priority_queue<int, vector<int>, greater<int>>pq;// min heap
+
 int size;
     KthLargest(int k, vector<int>& nums) {
         size=k;
-       for(int i =0;i<nums.size();i++)
-       {
-           pq.push(nums[i]);
-           if(pq.size()>k){
-               pq.pop();
-           }
-       } 
+        for(int i =0; i<nums.size();i++){
+            pq.push(nums[i]);
+            if(pq.size()>k){
+                pq.pop();
+            }
+        }
     }
     
     int add(int val) {
         pq.push(val);
-         if(pq.size()>size){
-               pq.pop();
-           }
-    return pq.top();
+        if(pq.size()>size){
+            pq.pop();
+        }
+        return pq.top();
     }
-
 };
+
+// tc -> O(N)+O(logn) ~ O(N) , O(N) for kthlargest constructor and O(logn) for add function of priority queue is it is internally using binary heap
+// space complexicity -> O(K)
 
 /**
  * Your KthLargest object will be instantiated and called as such:
